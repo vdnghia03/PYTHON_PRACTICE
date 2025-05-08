8cee6e98a1104173c24feee4298293b34bbff9e7
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-05-08 18:03:27 +0000

    daemon and proof

--- MultiThreading_Asycio/docs_thread.md
+++ MultiThreading_Asycio/docs_thread.md
@@ -9,4 +9,10 @@
     - args: đối số của task
     - deamon: chạy ngầm, khi Main chạy xong thì main nó xóa đi các thread chạy ngầm
 - Các thread trong chương trình thực thi đồng thời - concurrent, tùy vào task từng thread mà có thời gian thực thi khác nhau
-- 
+
+- Hiểu về daemon:
+    - Khi đặt daemon = True, thì rõ ràng khi Main thread chạy xong nó sẽ kill cái thread phụ. Ta không muốn điều này, vì vậy ta thêm lệnh sub_thread.join(). Tưởng tượng như anh MainThread tới trước cổng rồi, tuy nhiên ảnh không được vào nhà, ảnh phải đợi sub_thread đi vào trước và sau đó Main thread mới được đi vào :))
+    - Ở đây sẽ có thắc mắc, là nếu vậy thì ngay từ đầu khỏi đặt daemon luôn, chương trình vẫn sẽ thực thi xong main thread và sau đó thực thi sub_thread mới đóng chương trình, khỏi mắc công khai báo deamon rồi sau đó lại phải dùng join củng bất tiện. Tuy nhiên cần phân biệt là với việc không đặt daemon = True, thì khác nhau ở chỗ ví dụ khi main_thread đến cánh cửa, nó đi qua luôn tuy nhiên nó không đóng lại và vẫn mở cho các anh khác vào. Còn nếu mà đặt deamon = True thì anh main thread ảnh chơi xấu, ảnh đóng lại luôn. Còn dùng join thì anh main thread đến cửa rồi thì bị trói lại, đợi khi nào các anh sau qua hết thì ảnh mới được qua và đóng chương trình.
+
+- Về anh Main Thread
+    - Với việc Main Thread qua cổng, đồng nghĩa với việc dù chương trình còn thực thi hay đóng lại, việc cập nhật Main thread sẽ không còn được nữa
--- MultiThreading_Asycio/using_thread_multithreading.py
+++ MultiThreading_Asycio/using_thread_multithreading.py
@@ -1,26 +1,85 @@
-import threading
-import time
+# ========================================
+#  Hiểu về MultiThread và Asycio Await
+# ========================================
+#
+# Các tác vụ trên Thread
+#
+# Chứng minh thread chạy concurrent
+#
+# ========================================
 
 
-def inc_number(number):
-    time.sleep(3)
-    number += 1
-    print(f'Number is {number}')
+# ================= Thread ===================
 
-def dosomething(number):
-    print(f'This is main thread')
 
-# inc_number(99)
+# import threading
+# import time
 
-# Làm mới thread - Thread() có 3 parametter 
-# gồm target-công việc muốn thực thi, 
-# args-parametter của target dạng tuple, 
-my_thread = threading.Thread(target = inc_number, args = (99,), daemon = True)
-my_thread.start()
+
+# # Dat gia tri
+
+# gia_tri = 100
+
+# def inc_number(number):
+#     global gia_tri
+#     time.sleep(3)
+#     number += 1
+#     gia_tri = number
+#     print(f'Number is {number}')
+
+# def dosomething(number):
+#     print(f'This is main thread')
+
+# # inc_number(99)
+
+# # Làm mới thread - Thread() có 3 parametter 
+# # gồm target-công việc muốn thực thi, 
+# # args-parametter của target dạng tuple, 
+# my_thread = threading.Thread(target = inc_number, args = (1,), daemon = True)
+# my_thread.start()
 
 # my_thread.join()
 
-dosomething(1000)
+# dosomething(1000)
 
-# Lấy ra current thread đang chạy
-print(threading.current_thread().name)
+# # Lấy ra current thread đang chạy
+# print(threading.current_thread().name)
+
+# print(f"Gia tri cua bien gia tri la {gia_tri}")
+
+
+
+# ====================== Chung minh thread concurrent =================
+
+import threading
+import time
+
+def sum_exponent(list, thread_name):
+    start_time = time.perf_counter()
+    sum = 0
+    for x in list:
+        time.sleep(0.001)
+        sum += x ** x
+
+    print(f'Thread "{thread_name}" says sum exponent is: {sum}')
+    print(f'Duration: {time.perf_counter() - start_time} - "{thread_name}"')
+
+myList = [x for x in range(1, 101)]
+
+# Start duration
+
+start_duration = time.perf_counter()
+
+my_thread = threading.Thread(target=sum_exponent, args=([x for x in range(10, 50)], 'Hoa'))
+your_thread = threading.Thread(target=sum_exponent, args=([x for x in range(2, 78)], 'Minh'))
+her_thread = threading.Thread(target=sum_exponent, args=(myList, 'Thuy'))
+
+my_thread.start()
+your_thread.start()
+her_thread.start()
+
+my_thread.join()
+your_thread.join()
+her_thread.join()
+
+print(f"Duration ALL:  {time.perf_counter() - start_duration}")
